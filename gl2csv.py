8bb3d33705d998f93799018f88edecab1f861592
Tom Gross <itconsense@gmail.com>
2017-05-21 20:27:12 +0000

    Count weekly closed issues and plot result

--- gl2csv.py
+++ gl2csv.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
 import argparse
 import csv
 import gitlab
 import pickle
+import iso8601
+import matplotlib.pyplot as plt
+from collections import deque
+import numpy
 
 # private token authentication
 
@@ -33,13 +38,15 @@ def main():
     gl = gitlab.Gitlab(args.url, args.token)
     #issues =  gl.project_issues.list(project_id=25,all=True)
     #pickle.dump(issues, open( "save.p", "wb" ) )
-    #print('Saved issues')
+    print('Saved issues')
     issues = pickle.load(open('save.p', 'rb'))
-    fieldnames = ['iid', 'id', 'title', 'state', 'created_at', 'updated_at', 'closed_at']
+    fieldnames = ['iid', 'id', 'title', 'state', 'created_at', 'updated_at', 'closed_at', 'closed_week']
     f = open('issues.csv', 'w')
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
     issue_number = len(issues)
+    all_issues = []
+    weeks = {w: 0 for w in range(1, 53)}
     for i, issue in enumerate(issues):
         print('{:4d}/{:4d}'.format(i, issue_number))
         d = {key: enc(val) for key, val in issue.as_dict().items()
@@ -52,7 +59,9 @@ def main():
         if d['state'] == 'closed':
             c = [x.created_at for x in notes if 'closed' in x.body.lower()]
             if c:
-                d['closed_at'] = c[0].encode('utf-8')
+                closed_at = c[0].encode('utf-8')
+                d['closed_at'] = closed_at
+                d['closed_week'] = iso8601.parse_date(closed_at).isocalendar()[1]
             else:
                 bodys = [x.body.lower() for x in notes if isinstance(x.body, unicode) and 'moved' in x.body.lower()]
                 if bodys:
@@ -62,10 +71,29 @@ def main():
                     print 
                     print(bodys)
                     d['closed_at'] = d['updated_at']
+                    d['closed_week'] = iso8601.parse_date(d['updated_at']).isocalendar()[1]
         else:
             d['closed_at'] = ''
-        writer.writerow(d)
-    f.close()
+            d['closed_week'] = ''
+        #writer.writerow(d)
+        all_issues.append(d)
+        if d['closed_week']:
+            weeks[d['closed_week']] += 1
+    print(weeks)
+    issues_count = weeks.values()
+    print('Minimum gelöst: {0} '.format(min([x for x in issues_count if x])))
+    print('Maximum gelöst: {0} '.format(max(issues_count)))
+    print('Median: {0} '.format(numpy.median(issues_count)))
+    print('Mittelwert: {0} '.format(numpy.average(issues_count)))
+    # 17 is because we started at week #36 in 2016
+    # 15 are the zero weeks  19.5.2017
+    wX = weeks.keys()
+    wY = issues_count
+    plt.plot(wX, wY)
+    plt.xlabel(u'Woche')
+    plt.ylabel(u'gelöste Issues')
+    plt.show()
+    #f.close()
 
 if __name__ == '__main__':
     main()
